aaf785eda9649b939ca7f14c4b80a5a06162f74b
muddassir19 <muhammadmuddassir19@gmail.com>
2022-07-16 17:33:57 +0530

    groovy file and jenkins file updtaed

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,12 @@
+def call(tomcatIP,warName,credId) {
+
+     sshagent(["${credId}"]) {
+             
+        sh "scp -o StrictHostKeyChecking=no  target/*.war ec2-user@${tomcatIp}:/opt/tomcat8/webapps/${warName}.war"
+        sh "ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/shutdown.sh"
+        sh "ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/startup.sh"
+      }
+}
+
+
+    
